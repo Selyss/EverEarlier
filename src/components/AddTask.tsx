f04fa63e0ae7e654005702c4eb913793c6b6a20c
Selyss <99344963+Selyss@users.noreply.github.com>
2024-01-01 19:03:47 -0500

    simple form with validation and toast

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,24 +1,12 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
+
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
 import {
   Form,
   FormControl,
@@ -29,168 +17,46 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { toast } from "@/components/ui/use-toast";
-import { cn, useMediaQuery } from "@/lib/utils";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as React from "react";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
 
-const FormSchema = z.object({
-  name: z.string({
-    required_error: "Please select a name for the task.",
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters long.",
   }),
-  priority: z.string({
-    required_error: "Please select a priority for the task.",
-  }),
-  progress: z.string({
-    required_error: "Please select your current progress.",
-  }),
+  // description: z.string().min(2, { // TODO: impl later
+  //   message: "Description must be at least 2 characters long.",
+  // }),
 });
-export function AddTodoTask() {
-  const isDesktop = useMediaQuery("(min-width: 768px)");
 
-  if (isDesktop) {
-    return <ProfileForm />;
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button variant="outline">Add Task</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Add task</DialogTitle>
-            <DialogDescription>
-              Add a new task to your list. Click save when you're done.
-            </DialogDescription>
-          </DialogHeader>
-          <ProfileForm />
-        </DialogContent>
-      </Dialog>
-    );
-  }
-
-  return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline">Add Task</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>Add task</DrawerTitle>
-          <DrawerDescription>
-            Add a new task to your list. Click save when you're done.
-          </DrawerDescription>
-        </DrawerHeader>
-        <ProfileForm className="px-4" />
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  );
-}
-
-function ProfileForm({ className }: React.ComponentProps<"form">) {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
+export function TaskForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      // description: "",
+    },
   });
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    toast("Task created: " + values.title);
   }
+
   return (
     <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className={cn("grid items-start gap-4", className)}
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="name"
+          name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Email</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a verified email to display" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="m@example.com">m@example.com</SelectItem>
-                  <SelectItem value="m@google.com">m@google.com</SelectItem>
-                  <SelectItem value="m@support.com">m@support.com</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormDescription>
-                You can manage email addresses in your{" "}
-              </FormDescription>
+              <FormLabel>Task Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Clean Room" {...field} />
+              </FormControl>
+              <FormDescription>This is the name of your task.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
         <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  );
-
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className={cn("grid items-start gap-4", className)}
-      >
-        <div className="grid gap-2">
-          <Label htmlFor="title">Name</Label>
-          <Input type="text" id="title" defaultValue="Clean Room" />
-        </div>
-        <div className="grid grid-cols-2 gap-2">
-          <div>
-            <Label htmlFor="priority">Priority</Label>
-            <Select name="priority">
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Task Priority" />
-              </SelectTrigger>
-              <SelectContent id="priority">
-                <SelectItem value="light">Low</SelectItem>
-                <SelectItem value="dark">Medium</SelectItem>
-                <SelectItem value="system">High</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <Label htmlFor="progress">Progress</Label>
-            <Select name="progress">
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Current Progress" />
-              </SelectTrigger>
-              <SelectContent id="progress">
-                <SelectItem value="light">To Do</SelectItem>
-                <SelectItem value="dark">In Progress</SelectItem>
-                <SelectItem value="system">Completed</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-        <Button type="submit">Save changes</Button>
       </form>
     </Form>
   );
