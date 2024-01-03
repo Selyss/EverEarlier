cda197b4711f1b746d746931bef422de57929d43
Selyss <99344963+Selyss@users.noreply.github.com>
2024-01-03 18:27:21 -0500

    removed checkboxes. add them back later under a modal option.

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -6,32 +6,9 @@ import { type ColumnDef } from "@tanstack/react-table";
 
 import TaskDropdown from "@/components/TaskDropdown";
 import { DataTableColumnHeader } from "@/components/table/ColumnHeader";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Progress } from "@/components/ui/progress";
 
 export const columns: ColumnDef<Task>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
   {
     accessorKey: "name",
     header: "Task",
