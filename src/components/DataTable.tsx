b7ec1028538937a1482ac8f532ceb140307a077f
Selyss <99344963+Selyss@users.noreply.github.com>
2024-01-02 20:46:29 -0500

    extract pagination

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { DataTablePagination } from "@/components/table/Pagination";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -172,24 +173,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
+      <DataTablePagination table={table} />
       <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
